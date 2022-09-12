77d6bdb32f804c4140e2a55d1903c73794c4a423
Subhav Mathur <subhav13@gmail.com>
2022-09-12 15:07:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FirstComp from './components/ex/FirstComp';
 import Counter from './components/counter/Counter';
+import DynamicTable from './components/dynamicTable/DynamicTable';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,7 +11,7 @@ class App extends Component {
   render(){
     return (
       <div className='App'>
-      <Counter/>
+      <DynamicTable/>
       </div>
     )
   }
